5b3339fbcbf4bd95bdbccb5e71105842c4d6b73b
applqpak <applqpak@gmail.com>
2016-07-31 16:56:21 +1000

    Create Main.java

---
+++ VPNGuard/Main.java
@@ -0,0 +1,19 @@
+package VPNGuard;
+
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.utils.Config;
+import cn.nukkit.utils.TextFormat;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.Player;
+
+import VPNGuard.libraries.Async;
+import VPNGuard.libraries.SimpleCache;
+import VPNGuard.libraries.CommandFunctions;
+
+public class Main extends PluginBase implements Listener
+{
+
+}
